c5509c9286b39df3ea680fe0dbf283108090f27e
D-Berg <danielfjberg@gmail.com>
2024-12-06 23:35:11 +0100

    example of enumerating adapters

---
+++ examples/enumerate_adapters/build.zig
@@ -0,0 +1,38 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "enumerate_adapters",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+        .strip = true
+    });
+
+    const zgl = b.dependency("zgl", .{ 
+        .target = target,
+        .optimize = optimize
+    });
+
+    exe.root_module.addImport("zgl", zgl.module("zgl"));
+
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    // This allows the user to pass arguments to the application in the build
+    // command itself, like this: `zig build run -- arg1 arg2 etc`
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
+}
---
+++ examples/enumerate_adapters/src/main.zig
@@ -0,0 +1,21 @@
+const std = @import("std");
+const wgpu = @import("zgl").wgpu;
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    const allocator = gpa.allocator();
+
+    const instance = try wgpu.Instance.Create(&.{});
+    defer instance.Release();
+
+    const adapters = try instance.EnumerateAdapters(allocator);
+    defer allocator.free(adapters);
+
+    for (adapters) |adapter| {
+        const info = adapter.GetInfo();
+
+        info.logInfo();
+    }
+}
