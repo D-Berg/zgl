6173d3583f0f28abafbedc3be582f205598e2f5b
D-Berg <danielfjberg@gmail.com>
2024-12-19 11:06:53 +0100

    ported triangle to web(breaking), not all functions exist in emscripten

--- examples/triangle-web/src/main.zig
+++ examples/triangle-web/src/main.zig
@@ -4,191 +4,136 @@ const testing = std.testing;
 const log = std.log.scoped(.@"main");
 const zgl = @import("zgl");
 const wgpu = zgl.wgpu;
-const Surface = wgpu.Surface;
 const glfw = zgl.glfw;
+const builtin = @import("builtin");
+const os_tag = builtin.os.tag;
+const Allocator = std.mem.Allocator;
+
+const stdout = std.io.getStdOut().writer();
+const emscripten = std.os.emscripten;
 
 const WINDOW_WIDTH = 800;
 const WINDOW_HEIGHT = 600;
 const triange_shader = @embedFile("shaders/triangle.wgsl");
 
-fn onDeviceLost(reason: wgpu.Device.LostReason, message: [*c]const u8, user_data: ?*anyopaque) callconv(.C) void {
-    _ = user_data;
-    log.err("lost device. reason: {}, message: {s}", .{reason, message});
-}
+const App = struct {
+    window: glfw.Window,
+    instance: wgpu.Instance,
+    surface: wgpu.Surface,
+    adapter: wgpu.Adapter,
+    device: wgpu.Device,
+    queue: *wgpu.Queue,
+    render_pipeline: wgpu.RenderPipeline, 
+    allocator: Allocator,
 
-fn onError(tpe: wgpu.ErrorType, message: [*c]const u8, userdata: ?*anyopaque) callconv(.C) void {
-    _ = userdata;
-    log.err("WGPU encountered an error of type {s} with message: {s}", .{@tagName(tpe), message});
-}
+    fn init(allocator: Allocator) !App {
 
-fn onQueueWorkDone(status: wgpu.Queue.WorkDoneStatus, message: [*c]const u8, userdata: ?*anyopaque) callconv(.C) void {
-    _ = userdata;
-    // _ = message;
-    if (message != null) log.info("got a message: {s}", .{message});
-    log.info("Queued work finished with status: {s}", .{@tagName(status)});
-}
+        try glfw.init();
+        glfw.Window.hint(.{ .client_api = .NO_API, .resizable = true });
+        const window = try glfw.Window.Create(WINDOW_WIDTH, WINDOW_HEIGHT, "My window");
 
-pub fn main() !void {
+        const instance = try wgpu.Instance.Create(null);
 
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
+        const surface = try glfw.GetWGPUSurface(window, instance);
 
-    const allocator = gpa.allocator();
+        const adapter = try instance.RequestAdapter(null);
 
-    try glfw.init();
-    defer glfw.terminate();
+        const device = try adapter.RequestDevice(null);
 
+        const queue = try device.GetQueue();
 
-    glfw.Window.hint(.{ .client_api = .NO_API, .resizable = true });
-    const window = try glfw.Window.Create(WINDOW_WIDTH, WINDOW_HEIGHT, "My window");
-    defer window.destroy();
+        const code_desc = wgpu.ShaderModule.WGSLDescriptor{
+            .code = triange_shader,
+            .chain = .{ .next = null, .sType = .ShaderModuleWGSLDescriptor }
+        };
+        const shader_module = try device.CreateShaderModule(&.{
+            .nextInChain = &code_desc.chain
+        });
+        defer shader_module.Release();
 
-    const instance = try wgpu.Instance.Create(&.{});
-    defer instance.Release();
+        const blend_state = wgpu.BlendState{
+            .color = .{ 
+                .srcFactor = .SrcAlpha,
+                .dstFactor = .OneMinusSrcAlpha,
+                .operation = .Add,
+            },
+            .alpha = .{
+                .srcFactor = .Zero,
+                .dstFactor = .One,
+                .operation = .Add
+            }
+        };
 
-    const surface = try glfw.GetWGPUSurface(window, instance);
-    defer surface.Release();
+        const zero: u32 = 0;
 
-    const adapter = try instance.RequestAdapter(&.{ });
-    defer adapter.Release();
+        const color_target = wgpu.ColorTargetState{
+            .format = .Undefined,
+            .blend = &blend_state,
+            .writeMask = .All
+        };
 
-    const surface_capabilities = surface.GetCapabilities(adapter);
-    defer surface_capabilities.FreeMembers();
+        const render_pipeline = try device.CreateRenderPipeline(&.{
+            .vertex = .{
+                .module = shader_module._impl,
+                .entryPoint = "vs_main",
+            },
+            .primitive = .{
+                .topology = .TriangleList,
+                .stripIndexFormat = .Undefined,
+                .frontFace = .CCW, //counter clockwise
+                .cullMode = .None,
+            },
+            .fragment = &.{
+                .module = shader_module._impl,    
+                .entryPoint = "fs_main",
+                .targetCount = 1,
+                .targets = &[1]wgpu.ColorTargetState{ color_target }
+            },
+            .multisample = .{
+                .count = 1,
+                .mask = ~zero,
+                .alphaToCoverageEnabled = false
+            },
+        });
 
-    surface_capabilities.logCapabilites();
+        return .{
+            .window = window,
+            .instance = instance,
+            .surface = surface,
+            .adapter = adapter,
+            .device = device,
+            .queue = queue,
+            .render_pipeline = render_pipeline,
+            .allocator = allocator,
+        };
 
-    if (adapter.GetLimits()) |limits| {
-        limits.logLimits();
     }
 
-    const adapter_features = try adapter.GetFeatures(allocator);
-    defer adapter_features.deinit();
-    adapter_features.logFeautures();
+    fn deinit(self: *App) void {
+        defer glfw.terminate();
+        defer self.window.destroy();
+        defer self.instance.Release();
+        defer self.surface.Release();
+        defer self.adapter.Release();
+        defer self.device.Release();
+        defer self.queue.Release();
+        defer self.render_pipeline.Release();
 
-    const adapter_info = adapter.GetInfo();
-    adapter_info.logInfo();
+    }
 
-    const device = try adapter.RequestDevice(&.{ 
-        .label = "My device", 
-        .defaultQueue = .{.label = "The default Queue"},
-        .deviceLostCallback = &onDeviceLost,
-        .uncapturedErrorCallbackInfo = .{.callback = &onError}
+    fn loop(ctx: ?*anyopaque) callconv(.C) void {
+        const self = @as(*App, @alignCast(@ptrCast(ctx)));
+        const surface = self.surface;
+        const device = self.device;
+        const queue = self.queue;
+        const render_pipeline = self.render_pipeline;
 
-    });
-    defer device.Release();
-    
-    const device_features = try device.GetFeatures(allocator);
-    defer device_features.deinit();
-    device_features.logFeautures();
-
-    const device_limits = try device.GetLimits();
-    device_limits.logLimits();
-
-    const queue = try device.GetQueue();
-    defer queue.Release();
-
-    queue.OnSubmittedWorkDone(&onQueueWorkDone, null);
-    // command_encoder.InsertDebugMarker("do first thing");
-    // command_encoder.InsertDebugMarker("do second thing");
-    // const command_buffer = command_encoder.Finish(null);
-    //
-    // queue.Submit(1, &command_buffer);
-    // command_buffer.Release();
-
-    var surface_config = Surface.Configuration{
-        .device = device._inner,
-        .width = WINDOW_WIDTH,
-        .height = WINDOW_HEIGHT,
-        .usage = .RenderAttachment,
-        .format = surface_capabilities.formats.?[0],
-        .presentMode = .Undefined,
-        .alphaMode = .Auto
-    };
-    surface.Configure(&surface_config);
-    defer surface.Unconfigure();
-
-    const code_desc = wgpu.ShaderModule.WGSLDescriptor{
-        .code = triange_shader,
-        .chain = .{ .next = null, .sType = .ShaderModuleWGSLDescriptor }
-    };
-    const shader_module = try device.CreateShaderModule(&.{
-        .nextInChain = &code_desc.chain
-    });
-    defer shader_module.Release();
-
-    const blend_state = wgpu.BlendState{
-        .color = .{ 
-            .srcFactor = .SrcAlpha,
-            .dstFactor = .OneMinusSrcAlpha,
-            .operation = .Add,
-        },
-        .alpha = .{
-            .srcFactor = .Zero,
-            .dstFactor = .One,
-            .operation = .Add
-        }
-    };
-
-    const zero: u32 = 0;
-
-    const color_target = wgpu.ColorTargetState{
-        .format = surface_capabilities.formats.?[0],
-        .blend = &blend_state,
-        .writeMask = .All
-    };
-
-    const render_pipeline = try device.CreateRenderPipeline(&.{
-        .vertex = .{
-            .module = shader_module._impl,
-            .entryPoint = "vs_main",
-        },
-        .primitive = .{
-            .topology = .TriangleList,
-            .stripIndexFormat = .Undefined,
-            .frontFace = .CCW, //counter clockwise
-            .cullMode = .None,
-        },
-        .fragment = &.{
-            .module = shader_module._impl,    
-            .entryPoint = "fs_main",
-            .targetCount = 1,
-            .targets = &[1]wgpu.ColorTargetState{ color_target }
-        },
-        .multisample = .{
-            .count = 1,
-            .mask = ~zero,
-            .alphaToCoverageEnabled = false
-        },
-    });
-    defer render_pipeline.Release();
-            
-
-
-    while (!window.ShouldClose()) {
-
-        glfw.pollEvents();
-
-        const texture = surface.GetCurrentTexture() catch |err| switch (err){
-            error.RecoverableTexture => {
-
-                const size = try window.GetSize();
-
-                surface_config.width = size.width;
-                surface_config.height = size.height;
-
-                surface.Configure(&surface_config);
-
-                continue;
-
-            },
-            else => {
-                return err;
-            }
-
+        const texture = surface.GetCurrentTexture() catch {
+            @panic("failed to get texture");
         };
         defer texture.Release();
 
-        const view = try texture.CreateView(&.{
+        const view = texture.CreateView(&.{
             .label = "Surface texture view",
             .format = texture.GetFormat(),
             .dimension = .@"2D",
@@ -197,7 +142,9 @@ pub fn main() !void {
             .baseArrayLayer = 0,
             .arrayLayerCount = 1,
             .aspect = .All,
-        });
+        }) catch { 
+            @panic("failed to get view");
+        };
         defer view.Release();
 
 
@@ -213,13 +160,15 @@ pub fn main() !void {
         };
 
         {
-            const render_pass_encoder = try command_encoder.BeginRenderPass(&.{
+            const render_pass_encoder = command_encoder.BeginRenderPass(&.{
                 .colorAttachmentCount = 1,
                 .colorAttachments = &.{render_pass_color_attachement},
                 .depthStencilAttachment = null,
                 .occlusionQuerySet = null,
                 .timestampWrites = null,
-            });
+            }) catch {
+                @panic("Failed to get render pass encoder");
+            };
 
             render_pass_encoder.SetPipeline(render_pipeline);
             render_pass_encoder.Draw(3, 1, 0, 0);
@@ -235,10 +184,31 @@ pub fn main() !void {
         queue.Submit(&.{command_buffer});
         command_buffer.Release();
 
-        surface.Present();
-
-        _ = device.Poll(false, null);  
     }
 
+    fn shouldClose(self: App) bool {
+        return self.window.ShouldClose();
+    }
+
+};
+
+pub fn main() !void {
+    try stdout.print("hello from zig\n", .{});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    const allocator = gpa.allocator();
+
+    var app = try App.init(allocator);
+    defer app.deinit();
+
+    if (os_tag == .emscripten) {
+        emscripten.emscripten_set_main_loop_arg(App.loop, &app, 0, @intFromBool(true));
+    } else {
+        while (!app.shouldClose()) {
+            glfw.pollEvents();
+        }
+    }
     
 }
+
