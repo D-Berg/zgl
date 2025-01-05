38618b517953e2bfe525d46b6b41a5f4b90d9b6c
D-Berg <danielfjberg@gmail.com>
2025-01-05 16:00:56 +0100

    wgpu update enum values of CompareFunction

--- src/wgpu/wgpu.zig
+++ src/wgpu/wgpu.zig
@@ -192,11 +192,11 @@ pub const CompareFunction = enum(u32) {
     Undefined = 0x00000000,
     Never = 0x00000001,
     Less = 0x00000002,
-    LessEqual = 0x00000003,
-    Greater = 0x00000004,
-    GreaterEqual = 0x00000005,
-    Equal = 0x00000006,
-    NotEqual = 0x00000007,
+    Equal = 0x00000003,
+    LessEqual = 0x00000004,
+    Greater = 0x00000005,
+    NotEqual = 0x00000006,
+    GreaterEqual = 0x00000007,
     Always = 0x00000008,
     Force32 = 0x7FFFFFFF
 };
