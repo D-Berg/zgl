7d044c48300cbd26da9f04afb68dc14b4dc5ef5b
D-Berg <danielfjberg@gmail.com>
2025-01-12 15:12:45 +0100

    Generate docs with zig build docs

--- build.zig
+++ build.zig
@@ -29,6 +29,24 @@ pub fn build(b: *std.Build) void {
         buildNative(b, zgl, target, optimize);
     }
 
+    // TODO:: find out standard way to expose a lib/package for others
+    const lib = b.addStaticLibrary(.{
+        .name = "zgl",
+        .root_source_file = b.path("src/zgl.zig"),
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+
+    const install_docs = b.addInstallDirectory(.{
+        .source_dir = lib.getEmittedDocs(),
+        .install_dir = .prefix,
+        .install_subdir = "docs",
+    });
+
+    const docs_step = b.step("docs", "Install docs into zig-out/docs");
+    docs_step.dependOn(&install_docs.step);
+
 }
 
 ///https://github.com/floooh/sokol-zig/blob/master/build.zig#L409
